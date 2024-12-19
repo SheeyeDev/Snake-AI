800677ab42c420156723bfd5eb6485e4f30bcf70
SheeyeDev <102896476+SheeyeDev@users.noreply.github.com>
2024-12-20 00:46:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+from generationManager import GenerationManager
+from config import *
+from boardRenderer import *
+from tile import *
+pygame.init()
+ 
+FramePerSec = pygame.time.Clock()
+ 
+displaysurface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Snake AI")
+
+ALL_SPRITES = pygame.sprite.Group()
+
+gm = GenerationManager(ALL_SPRITES)
+gm.prepare()
+
+out = 0
+while True:
+
+    # Wait till previous turn ended, make next turn
+    if out==0:
+        out = 1    
+        out = gm.loop()
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+     
+    displaysurface.fill(BACKGROUND_COLOR)
+    
+    # Display sprites and text
+    for entity in ALL_SPRITES:
+        displaysurface.blit(entity.surf, entity.rect)
+
+    for holder in ALL_TEXT:
+        displaysurface.blit(holder.text, holder.position)
+ 
+    pygame.display.update()
+    FramePerSec.tick(FPS)
